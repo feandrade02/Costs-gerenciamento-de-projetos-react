8fbb8557760ebe7900b745cc09e14427d7536d13
feandrade02 <fe.a.andrade@hotmail.com>
2024-08-05 20:22:22 -0300

    incrementando página da empresa

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,6 +1,26 @@
-function Company(){
+import styles from './Company.module.css'
+
+import Container from '../layout/Container'
+
+function Company() {
     return(
-        <h1>Company</h1>
+        <div>
+            <h1>Conheça nossa história</h1>
+            <div className={styles.quadro}>
+                <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit 
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                    occaecat cupidatat non proident, sunt in culpa qui officia 
+                    deserunt mollit anim id est laborum."
+                </p>
+                <img src='https://placehold.co/600x400' alt='Foto'/>
+            </div>
+        </div>
     )
 }
+
 export default Company
